9f97e944e3fedc02a81872109b87d4cef5d99457
KajetanFrackowiak <kajtek.gdynia@gmail.com>
2024-06-03 22:26:26 +0200

    .

---
+++ build/CMakeFiles/writepng.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/writepng.dir/writepng.c.o"
+  "CMakeFiles/writepng.dir/writepng.c.o.d"
+  "writepng"
+  "writepng.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/writepng.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/writepng.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for writepng.
+# This may be replaced when dependencies are built.
